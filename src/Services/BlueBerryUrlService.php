f2c7d7636a8c0e71cd2ee84b860cd8b67717e1ed
Sorin Popescu <sorin@intelipromo.ro>
2019-02-21 23:44:01 +0200

    Typo

--- src/Services/BlueBerryUrlService.php
+++ src/Services/BlueBerryUrlService.php
@@ -3,14 +3,15 @@
 namespace BlueBerry\Services;
 
 //use IO\Services\UrlService;
-use Plenty\Plugin\Routing\Router;
+use Plenty\Plugin\Routing\Route;
+//use Illuminate\Routing\Route;
 
 class BlueBerryUrlService {
 
     //private $urlService;
     private $routeService;
 
-    public function __construct(Router $routeService) {//UrlService $urlService,
+    public function __construct(Route $routeService) {//UrlService $urlService,
         //$this->urlService = $urlService;
         $this->routeService = $routeService;
     }
