3128906c48f83a27f1caf4d1fd68a759f9db4f44
Sorin Popescu <sorin.popescu@skin.ro>
2019-02-22 11:47:21 +0200

    Updates for Scripts

--- resources/js/blue-berry.js
+++ resources/js/blue-berry.js
@@ -1,9 +1,11 @@
 // BlueBerry Scripts
-$(document).ready(function() {
+(function($) {
+
     // Alter register link
     $('.login a').each(function() {
         if ($(this).attr('href') === '/register') {
             $(this).attr('href') = 'customer-register';
         };
     });
-});
+
+})(jQuery);
