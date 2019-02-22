8097123f70610d15568005e9559e17cb78d67626
Sorin <iteam.soft@gmail.com>
2019-02-22 09:25:48 +0200

    set namespace name

--- src/Services/BlueBerryUrlService.php
+++ src/Services/BlueBerryUrlService.php
@@ -4,19 +4,19 @@ namespace BlueBerry\Services;
 
 //use IO\Services\UrlService;
 //use Plenty\Plugin\Routing\Route;
-use Illuminate\Routing\Route;
+use Illuminate\Routing\Route as rootRoute;
 
 class BlueBerryUrlService {
 
     //private $urlService;
     private $routeService;
 
-    public function __construct(Route $routeService) {//UrlService $urlService,
+    public function __construct() {//UrlService $urlService,Route $routeService
         //$this->urlService = $urlService;
         //$this->routeService = $routeService;
     }
 
     public function getCurrentController() {
-        die('x'.Route::getCurrentRoute()->getActionName());
+        die('x'.rootRoute::getCurrentRoute()->getActionName());
     }
 };
