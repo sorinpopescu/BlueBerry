bd391d49b5853cedb428b66a095df46a5c23ae3d
Sorin Popescu <sorin@intelipromo.ro>
2019-03-12 22:24:00 +0200

    Routes updated

--- src/Providers/BlueBerryRouteServiceProvider.php
+++ src/Providers/BlueBerryRouteServiceProvider.php
@@ -10,7 +10,13 @@ class BlueBerryRouteServiceProvider extends RouteServiceProvider {
     public function map(Router $router) {
         // Login action
         $router->get('customer-login','BlueBerry\Controllers\ContentController@doLogin');
+        $router->get('de/customer-login','BlueBerry\Controllers\ContentController@doLogin');
+        $router->get('en/customer-login','BlueBerry\Controllers\ContentController@doLogin');
+        $router->get('gb/customer-login','BlueBerry\Controllers\ContentController@doLogin');
         // Register action
         $router->get('customer-register','BlueBerry\Controllers\ContentController@doRegister');
+        $router->get('de/customer-register','BlueBerry\Controllers\ContentController@doRegister');
+        $router->get('en/customer-register','BlueBerry\Controllers\ContentController@doRegister');
+        $router->get('gb/customer-register','BlueBerry\Controllers\ContentController@doRegister');
     }
 }
