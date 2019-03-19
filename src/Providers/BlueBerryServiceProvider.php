f5e8c8cfe2abb1b109bcf9d1e0ca88e49fc56d93
Sorin <2951110+sorinpopescu@users.noreply.github.com>
2019-03-19 09:34:31 +0200

    Ha - boot on

--- src/Providers/BlueBerryServiceProvider.php
+++ src/Providers/BlueBerryServiceProvider.php
@@ -23,7 +23,7 @@ class BlueBerryServiceProvider extends ServiceProvider {
         $this->getApplication()->singleton( BlueBerryUrlService::class );
     }
 
-    public function nooot(Twig $twig, Dispatcher $eventDispatcher) {
+    public function boot(Twig $twig, Dispatcher $eventDispatcher) {
         // Get the service data
         $customerService = pluginApp(BlueBerryCustomerService::class);
         $urlService = pluginApp(BlueBerryUrlService::class);
