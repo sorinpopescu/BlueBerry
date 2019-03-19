8e4293d40ed6fe6985007067538fd6dec0e9a51b
Sorin Popescu <sorin.popescu@skin.ro>
2019-03-19 15:59:54 +0200

    Fixes212

--- src/Providers/BlueBerryServiceProvider.php
+++ src/Providers/BlueBerryServiceProvider.php
@@ -27,7 +27,7 @@ class BlueBerryServiceProvider extends ServiceProvider {
     public function boot(Dispatcher $eventDispatcher) {
         // Get the service data
         $customerService = pluginApp(AccountService::class);
-        die('X'.$_SERVER['REQUEST_URI']);
+        echo 'X'.$_SERVER['REQUEST_URI'];
         // $urlService = pluginApp(Request::class);
         /// $currentUri = $urlService->getRequestUri();
         // // What language
