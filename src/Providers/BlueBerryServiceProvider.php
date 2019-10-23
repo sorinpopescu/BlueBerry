c1b5848fdd416626aaa367448a67399b0b9a8e22
Cristian GHeorghe <cristtian.gheorghe@gmail.com>
2019-10-23 15:30:49 +0200

    update header

--- src/Providers/BlueBerryServiceProvider.php
+++ src/Providers/BlueBerryServiceProvider.php
@@ -38,7 +38,7 @@ class BlueBerryServiceProvider extends ServiceProvider {
 
         $eventDispatcher->listen('IO.init.templates', function (Partial $partial){
             $partial->set('header', 'BlueBerry::content.header');
-
+            return false;
         }, 0);
 
 
