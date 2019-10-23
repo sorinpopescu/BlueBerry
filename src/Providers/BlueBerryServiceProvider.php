4c064abe6ed89a1b579c812f9d9d635fcf0cc88f
Cristian GHeorghe <cristtian.gheorghe@gmail.com>
2019-10-23 15:05:32 +0200

    update to see if working

--- src/Providers/BlueBerryServiceProvider.php
+++ src/Providers/BlueBerryServiceProvider.php
@@ -33,14 +33,15 @@ class BlueBerryServiceProvider extends ServiceProvider {
         $eventDispatcher->listen('IO.Resources.Import', function (ResourceContainer $container)
         {
             // The style is imported in the <head> on the PageDesign.twig of Ceres
-            $container->addStyleTemplate('BlueBerry::content.styles');
+            $container->addStyleTemplate('BlueBerry::content.styless');
         }, self::EVENT_LISTENER_PRIORITY);
 
         $eventDispatcher->listen('IO.init.templates', function (Partial $partial){
             $partial->set('header', 'BlueBerry::content.header');
+            return false;
         }, 0);
 
-        return false;
+
     }
 
     /**
