f4d7bbdedad865a1305b003a9f7a05a4155e9689
Cristian GHeorghe <cristtian.gheorghe@gmail.com>
2019-10-24 11:56:39 +0200

    bb-24102019-25

--- src/Providers/BlueBerryServiceProvider.php
+++ src/Providers/BlueBerryServiceProvider.php
@@ -34,14 +34,10 @@ class BlueBerryServiceProvider extends ServiceProvider {
         $eventDispatcher->listen('IO.init.templates', function(Partial $partial)
         {
            $partial->set('footer', 'BlueBerry::content.ThemeFooter');
-            return false;
-        }, 0);
-        $eventDispatcher->listen('IO.tpl.home', function (TemplateContainer $container)
-            {
-                $container->setTemplate('Ceres::Homepage.Homepage');
-                return false;
-            }, 0);
 
+        }, 0);
+
+        return false;
     }
 
     /**
