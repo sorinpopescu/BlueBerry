e04a7e984aca972f3bdcbb24a4c65116b36ac748
Cristian GHeorghe <cristtian.gheorghe@gmail.com>
2019-10-24 11:41:05 +0200

    bb-24102019-22

--- src/Providers/BlueBerryServiceProvider.php
+++ src/Providers/BlueBerryServiceProvider.php
@@ -38,10 +38,10 @@ class BlueBerryServiceProvider extends ServiceProvider {
         $eventDispatcher->listen('IO.init.templates', function(Partial $partial)
         {
            $partial->set('footer', 'BlueBerry::content.ThemeFooter');
-
+            return false;
         }, 0);
 
-        return false;
+
     }
 
     /**
