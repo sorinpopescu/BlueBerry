a5c4e06b51af560ac6f17c82f0da378777b422ca
Cristian GHeorghe <cristtian.gheorghe@gmail.com>
2019-11-05 11:46:10 +0100

    update product view 05112019-15

--- src/Containers/BlueBerryItemListContainer1.php
+++ src/Containers/BlueBerryItemListContainer1.php
@@ -6,7 +6,7 @@ use Plenty\Plugin\Templates\Twig;
 
 class BlueBerryItemListContainer1
 {
-    public function call(Twig $twig):string
+    public function call(Twig $twig, $arg):string
     {
         return $twig->render('BlueBerry::Containers.ItemLists.ItemList1', ["item" => $arg[0]]);
     }
