25c44330adbf34d13a1f95005d3c860f3b9edbb5
Sorin <sorin@twn.ro>
2022-08-18 16:19:27 +0200

    Update 6

--- src/Providers/BlueBerryServiceProvider.php
+++ src/Providers/BlueBerryServiceProvider.php
@@ -46,7 +46,7 @@ class BlueBerryServiceProvider extends ServiceProvider {
     {
 
         $this->listenToIO("Resources.Import", $eventDispatcher, function(ResourceContainer $container) {
-            //$container->addScriptTemplate('BlueBerry::ItemList.Components.CategoryItem');
+            $container->addScriptTemplate('BlueBerry::ItemList.Components.CategoryItem');
             // $container->addScriptTemplate('BlueBerry::Item.Components.SingleItem');
             //$container->addScriptTemplate('BlueBerry::Item.Components.ItemPrice');
         });
